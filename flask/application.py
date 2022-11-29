faeeed55bafc36d21105446b1e8fc148753e8dc9
gradeten <88281367+gradeten@users.noreply.github.com>
2022-11-29 18:20:29 +0900

    Add files via upload

--- flask/application.py
+++ flask/application.py
@@ -1,14 +1,38 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
 from database import DBhandler
 import sys
+import math
 
 application = Flask(__name__)
+application.config["SECRET_KEY"] = "anything-you-want"
 
 DB = DBhandler()
 
 @application.route("/")
 def hello():
-    return render_template("index.html")
+    
+    data = DB.get_restaurants()
+
+    total=len(data)
+    
+    if total>=9:
+        keys=(list(data))[::-1][:9]
+    else:
+        keys=(list(data))[::-1]
+    
+    print(total)
+    
+    datas = [data[k] for k in keys]
+    
+    return render_template(
+        "index.html",
+        # datas = data.items(),
+        datas = datas,
+        for_len = range(len(keys))
+        
+    )
+
+
 
 @application.route("/favorite")
 def view_favorite():
@@ -28,7 +52,8 @@ def view_dinerform():
 
 @application.route("/dinerlist")
 def view_dinerlist():
-    return render_template("dinerlist.html")
+    # return render_template("dinerlist.html")
+    return redirect(url_for('list_restaurants'))
 
 @application.route("/placeDashboard")
 def view_placeDashboard():
@@ -36,7 +61,7 @@ def view_placeDashboard():
 
 @application.route("/review_upload")
 def view_reviewupload():
-    return render_template("review_upload.html")
+    return render_template("review_form.html")
 
 @application.route("/review")
 def view_review():
@@ -58,13 +83,14 @@ def reg_diner_submit():
     addr=request.args.get("addr")
     tel=request.args.get("tel")
     category=request.args.get("category")
+    price=request.args.get("price")
     park=request.args.get("park")
     time1=request.args.get("time1")
     time2=request.args.get("time2")
     site=request.args.get("site")
     file=request.args.get("file")
     
-    print(name,addr,tel,category,park,time1,time2,site, file)
+    print(name,addr,tel,category,price,park,time1,time2,site, file)
 
 
 @application.route("/submit_diner_post", methods=['POST'])
@@ -77,7 +103,8 @@ def reg_diner_submit_post():
     if DB.insert_restaurant(data['name'], data, image_file.filename):
         return render_template("submit_diner_result.html", data=data, image_path="static/image/"+image_file.filename)
     else:
-        return "Restaurant name already exist!"
+        flash("Restaurant name already exist!")
+        return redirect(url_for('view_dinerform'))
 
 
 @application.route("/submit_menu")
@@ -86,10 +113,9 @@ def reg_menu_submit():
     price=request.args.get("price")
     allergy=request.args.get("allergy")
     write=request.args.get("write")
-    picture=request.args.get("file")
+    file=request.args.get("file")
     
     print(name,price,allergy,write,file)
-
 
 @application.route("/submit_menu_post", methods=['POST'])
 def reg_menu_submit_post():
@@ -104,20 +130,82 @@ def reg_menu_submit_post():
 
 
 
-@application.route("/submit_review_post", methods=['POST'])
-def reg_review_submit_post():
 
-    image_file=request.files["file"]
+@application.route("/list")
+def list_restaurants():
+    page = request.args.get("page", 0, type=int)
+    limit = 9
+    
+    start_idx = limit*page
+    end_idx = limit*(page+1)
+    data = DB.get_restaurants()
+    # print(data)
+    tot_count = len(data)
+    # data = dict(list(data.items())[start_idx:end_idx])
+    data = list(data.items())[start_idx:end_idx]
+    page_count = len(data)
+    
+    return render_template(
+        "list.html",
+        # datas = data.items(),
+        datas = data,
+        total = tot_count,
+        limit = limit,
+        page = page,
+        page_count = math.ceil(tot_count/9),
+        real_page_count = str(int((tot_count/9)))
+    )
+
+@application.route("/view_detail/<name>/")
+def view_restaurant_detail(name):
+    data = DB.get_restaurant_byname(str(name))
+    print("####data:", data)
+    review = DB.get_review_by_res_name(str(name))
+    rAmount = len(review)
+    r_avg_rate = DB.get_avgrate_by_res_name(str(name))
+    menudata = DB.get_food_byname(str(name))
+    tot_count = len(menudata)
+    
+    return render_template("detail.html", data=data, review=review, r_avg_rate=r_avg_rate, menudata=menudata,
+        rAmount = rAmount, total=tot_count)
+
+@application.route("/review/<res_name>/")
+def restaurant_review(res_name):
+    review_data = DB.get_review_by_res_name(str(res_name))
+    r_avg_rate = DB.get_avgrate_by_res_name(str(res_name))
+    print("####data:",review_data)
+    return render_template("review.html", res_name=res_name, review_data=review_data, r_avg_rate=r_avg_rate)
+
+@application.route("/restaurants/<res_name>/new", methods=['GET'])
+def new_restaurant_review(res_name):
+    return render_template("review_form.html", res_name=res_name)
+
+@application.route("/restaurants/<res_name>/review", methods=['POST'])
+def create_review(res_name):
+    image_file = request.files["file"]
     image_file.save("static/image/{}".format(image_file.filename))
-    reviewdata=request.form
+    review_data = request.form
 
-    if DB.insert_review(reviewdata,image_file.filename):
-        return render_template("submit_review_result.html",reviewdata=reviewdata, image_path="static/image/"+image_file.filename)
+    if DB.insert_review(review_data, image_file.filename):
+        return redirect(url_for('restaurant_review', res_name=res_name))
+    
+    
+@application.route("/menu_list/<restaurant_name>/")
+def view_foods(restaurant_name):
+    
+    data = DB.get_food_byname(str(restaurant_name))
+    tot_count = len(data)
+    page_count = len(data)
 
+    return render_template(
+        "menu_list.html",
+        datas=data,
+        total=tot_count,
+        restaurant_name = restaurant_name
+    )
 
-
-
-
+    if DB.insert_review(review_data, image_file.filename):
+        return redirect(url_for('restaurant_review', res_name=res_name))
 
 
 if __name__ == "__main__":
--- flask/database.py
+++ flask/database.py
@@ -4,16 +4,18 @@ import json
 class DBhandler:
     def __init__(self):
         with open('./authentication/firebase_auth.json') as f:
-            config=json.load(f)
+            config = json.load(f)
             
         firebase = pyrebase.initialize_app(config)
         self.db = firebase.database()
     
     def insert_restaurant(self, name, data, img_path):
         restaurant_info = {
+            "name": name,
             "addr": data['addr'],
             "tel": data['tel'],
             "category": data['category'],
+            "price": data['price'],
             "park": data['park'],
             "time1": data['time1'],
             "time2": data['time2'],
@@ -21,16 +23,16 @@ class DBhandler:
             "img_path": img_path
         }
         if self.restaurant_duplicate_check(name):
-            self.db.child("restaurant").child(name).set(restaurant_info)
-            print(data,img_path)
+            self.db.child("restaurant").push(restaurant_info)
+            print(data, img_path)
             return True
         else:
             return False
-        
-        
-        
+
     def insert_menu(self, name, menudata, img_path):
         menu_info = {
+            "restaurant_name" : menudata["restaurant_name"],
+            "name" : menudata['name'],
             "price" : menudata['price'],
             "allergy" : menudata['allergy'],
             "write" : menudata['write'],
@@ -38,35 +40,83 @@ class DBhandler:
         }
         if self.menu_duplicate_check(name):
             self.db.child("menu").child(name).set(menu_info)
-            print(menudata,img_path)
+            print(menudata, img_path)
             return True
         else:
             return False
        
-    def menu_duplicate_check(self,name):
+    def menu_duplicate_check(self, name):
         menus = self.db.child("menu").get()
-        for me in menus.each():
-            if me.key() == name:
-                return False
-        return True   
-       
-    
-    
+        
+        if str(menus.val()) == "None":
+            return True
+        else:
+            for me in menus.each():
+                if me.key() == name:
+                    return False
+            return True   
+
     def restaurant_duplicate_check(self, name):
         restaurants = self.db.child("restaurant").get()
         for res in restaurants.each():
-            if res.key() == name:
+            value = res.val()
+            
+            if value['name'] == name:
                 return False
         return True
+    
+    def get_restaurants(self):
+        restaurants = self.db.child("restaurant").get().val()
+        return restaurants
+    
+    def get_restaurant_byname(self, name):
+        restaurants = self.db.child("restaurant").get()
+        target_value = ""
+        for res in restaurants.each():
+            value = res.val()
+            
+            if value['name'] == name:
+                target_value = value
+        return target_value
 
-
-    def insert_review(self,reviewdata,img_path):
-        review_info ={
-            "star":reviewdata['starRate'],
-            "review":reviewdata['review'],
-            "img_path":img_path
-
+    def insert_review(self, review_data, img_path):
+        review_info = {
+            "res_name": review_data['res_name'],
+            "star": review_data['star_rating'],
+            "review": review_data['review'],
+            "img_path": img_path,
+           # "created_at": datetime.now()
         }
         self.db.child("review").push(review_info)
-        print(reviewdata,img_path)
+        print(review_data, img_path)
         return True
+
+    def get_avgrate_by_res_name(self, res_name):
+        reviews = self.db.child("review").get()
+        rates = []
+        for review in reviews.each():
+            value = review.val()
+            if value['res_name'] == res_name:
+                rates.append(float(value['star']) if value['star'] != '' else 0)
+        return 0 if len(rates) == 0 else round(sum(rates) / len(rates), 1)
+
+    def get_review_by_res_name(self, res_name):
+        reviews = self.db.child("review").get()
+        target_value = []
+        for review in reviews.each():
+            value = review.val()
+            if value['res_name'] == res_name:
+                target_value.append(value)
+        return target_value
+    
+    
+    def get_food_byname(self, name):
+        restaurants = self.db.child("menu").get()
+        target_value=[]
+        for res in restaurants.each():
+            value = res.val()
+            print(value)
+
+            if value['restaurant_name'] == name:
+                target_value.append(value)
+        return target_value    
