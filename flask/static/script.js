c25dd8a3359917367674680c679e02e5484fe4cc
gradeten <88281367+gradeten@users.noreply.github.com>
2022-12-19 19:12:13 +0900

    Add files via upload

--- flask/static/script.js
+++ flask/static/script.js
@@ -1,18 +1,19 @@
 let popup = document.getElementById("popup");
 let check = false;
 
-    function openPopup(){
-        if (check == false)
-        {
-            popup.classList.add("open-popup");
-            check = true;
-        }
-        else
-        {
-            popup.classList.remove("open-popup");
-            check = false;
-        }
+function openPopup(){
+    if (check == false)
+    {
+        popup.classList.add("open-popup");
+        check = true;
     }
+    else
+    {
+        popup.classList.remove("open-popup");
+        check = false;
+    }
+}
+
 
   
 /*Slider*/
